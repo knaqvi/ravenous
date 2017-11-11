d044a067d0a655a07bfeaaccd22befcc202eeec0
Kashif Naqvi <kashif@Kashifs-MacBook-Pro.local>
2017-11-10 20:01:20 -0800

    Finished ravenous part 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BusinessList from './src/components/BusinessList/BusinessList';
+import SearchBar from './src/components/SearchBar/SearchBar.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div class="App">
+      <h1>ravenous</h1>
+        <SearchBar />, document.getElementById('app')
+        <BusinessList /> document.getElementById('app')
+</div>
     );
   }
 }
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -13,5 +13,24 @@ class SearchBar extends React.Component {
       let sortByOptionValue = sortByOptions[sortByOption];
       return <li key={sortByOption.sortByOptionValue}>{sortByOption}</li>
     });
+    render() {
+      return (
+        <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+        <ul>
+        {this.renderSortByOptions}
+        </ul>
+        </div>
+        <div className="SearchBar-fields">
+        <input placeholder="Search Businesses" />
+        <input placeholder="Where?" />
+        </div>
+        <div className="SearchBar-submit">
+        <a>"Let's Go"</a>
+      </div>
+      </div>
+      );
+    }
   }
 }
+export default SearchBar;
