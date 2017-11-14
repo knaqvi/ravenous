090a6d8d1ad011b5e101b04d9cb9952c30c4bb72
Kashif Naqvi <kashif@Kashifs-MacBook-Pro.local>
2017-11-14 00:31:37 -0800

    Ravenous III

--- src/App.js
+++ src/App.js
@@ -19,6 +19,9 @@ const business = {
 const businesses = [business, business, business, business, business, business];
 
 class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log('Searching Yelp with ${term}, ${location}, ${sortBy}');
+  }
   render() {
     return (
       <div className="App">
